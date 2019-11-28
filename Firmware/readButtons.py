80b21fd4422e778c59a98b88e9d096049201b09b
Oliver <othurn@ucsd.edu>
2019-11-27 17:08:52 -0800

    again trying to add images.py

--- Firmware/readButtons.py
+++ Firmware/readButtons.py
@@ -1,18 +1,18 @@
 from gpiozero import Button
 
-cButtonPin = 16
+cButtonPin = 13
 pButtonPin = 6
 
 cameraButton = Button(cButtonPin)
 powerButton = Button(pButtonPin)
 
-buttonPressed = false
+buttonPressed = False
 
-while !buttonPressed:
+while buttonPressed == False:
     if cameraButton.is_pressed:
         print("camera button pin 16")
-        buttonPressed = true
+        buttonPressed = True
     if powerButton.is_pressed:
         print("power button pin 6")
-        buttonPressed = true
+        buttonPressed = True
 
